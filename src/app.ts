2a8b3bee51dde510e3087a0affce9743a8df2a29
nixena <nikanozadze1010@gmail.com>
2023-05-10 12:13:31 +0400

    feat: connect mongo db

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectToMongo from "./config/mongo.js";
+
+dotenv.config();
+connectToMongo();
+
+const app = express();
+
+app.listen(process.env.PORT || 3000);
