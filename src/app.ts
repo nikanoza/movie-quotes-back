565ef2e9d0819a974996a29013db48ec73eb00c1
nixena <nikanozadze1010@gmail.com>
2023-07-20 13:00:03 +0400

    feat: create user router & add multer to project

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 import connectToMongo from "./config/mongo.js";
-import { authRouter } from "./routes";
+import { authRouter, userRouter } from "./routes";
 import swaggerMiddleware from "./middlewares/swagger-middleware.js";
 
 dotenv.config();
@@ -14,6 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api", authRouter);
+app.use("/api", userRouter);
 
 app.use("/", ...swaggerMiddleware);
 
---
+++ src/routes/user-router.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import multer, { FileFilterCallback } from 'multer';
+
+const userRouter = express.Router();
+
+export default userRouter;
