7d90c88c8a352624a03ce238947426e8a9017994
nixena <nikanozadze1010@gmail.com>
2023-07-20 13:05:37 +0400

    feat: create avatar update route with multer

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,6 +1,42 @@
-import express from "express";
+import express, { Request } from "express";
 import multer, { FileFilterCallback } from 'multer';
+import { DestinationCallback, FileNameCallback } from "types";
+
+const fileStorage = multer.diskStorage({
+    destination: (
+      _: Request,
+      __: Express.Multer.File,
+      callback: DestinationCallback
+    ): void => {
+      callback(null, 'public/storage')
+    },
+    filename: (
+      _: express.Request,
+      file: Express.Multer.File,
+      callback: FileNameCallback
+    ): void => {
+      callback(null, new Date().toISOString() + file.originalname)
+    },
+  })
+  
+  const fileFilter = (
+    __: express.Request,
+    file: Express.Multer.File,
+    callback: FileFilterCallback
+  ): void => {
+    if (
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
+    ) {
+      callback(null, true)
+    } else {
+      callback(null, false)
+    }
+  }
 
 const userRouter = express.Router();
+
+userRouter.put("/user/avatar", multer({ storage: fileStorage, fileFilter }).single('avatar'),)
 
 export default userRouter;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export * from "./models";
 export * from "./schemas";
+export * from "./multer";
