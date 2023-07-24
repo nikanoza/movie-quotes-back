d5fb816778fe015d9e6b836ae3413d3115581c6a
Nika Nozadze <nikanozadze1010@gmail.com>
2023-07-24 22:38:26 +0400

    wip: work with add movie validation schema

---
+++ src/schemas/movie-schemas/add-movie-schema.ts
@@ -0,0 +1,64 @@
+import Joi from "joi";
+import { User } from "../../models";
+import { CreateMovie, Name } from "types";
+
+const loginSchema = async (data: CreateMovie) => {
+  const commonSchema = Joi.object<Name>({
+    eng: Joi.string().required(),
+    geo: Joi.string().required(),
+  });
+
+  const nameSchema = Joi.object<Name>({
+    eng: Joi.string()
+      .required()
+      .external(async (value) => {
+        try {
+          const user = await User.findOne({
+            "name.eng": value,
+            id: data.userId,
+          });
+          if (user) {
+            throw new Error("movie with this name already exists");
+          }
+          return value;
+        } catch (error) {
+          throw new Error("Invalid credentials");
+        }
+      }),
+    geo: Joi.string()
+      .required()
+      .external(async (value) => {
+        try {
+          const user = await User.findOne({
+            "name.geo": value,
+            id: data.userId,
+          });
+          if (user) {
+            throw new Error("movie with this name already exists");
+          }
+          return value;
+        } catch (error) {
+          throw new Error("Invalid credentials");
+        }
+      }),
+  });
+
+  return Joi.object<CreateMovie>({
+    userId: Joi.string()
+      .required()
+      .external(async (value) => {
+        try {
+          const user = await User.findOne({ id: value });
+          if (!user) {
+            throw new Error("User did not fined");
+          }
+          return value;
+        } catch (error) {
+          throw new Error("Invalid credentials");
+        }
+      }),
+    name: nameSchema,
+  });
+};
+
+export default loginSchema;
