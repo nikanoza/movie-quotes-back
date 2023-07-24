1102af475871fa69fc11f49519d1512a4fdd5324
Nika Nozadze <nikanozadze1010@gmail.com>
2023-07-24 23:05:07 +0400

    feat: create movie post request function

---
+++ src/controllers/movie-controller.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import { addMovieSchema } from "../schemas";
+import { User } from "../models";
+
+import { v4 as uuid } from "uuid";
+
+export const addMovie = async (req: Request, res: Response) => {
+  try {
+    const { body } = req;
+    const paramsUserId = req.params.userId;
+    const { file } = req;
+
+    if (!file) {
+      return res.status(400).json({ message: "image did not upload" });
+    }
+
+    const validator = await addMovieSchema({
+      ...body,
+      userId: paramsUserId,
+      poster: "/storage/" + file.filename,
+    });
+
+    const { userId, name, categories, year, poster, director, description } =
+      await validator.validateAsync({
+        ...body,
+        userId: paramsUserId,
+        poster: "/storage/" + file.filename,
+      });
+
+    const user = await User.findOne({ id: userId });
+    const id = uuid();
+
+    if (user) {
+      user.movies.push({
+        name,
+        categories,
+        year,
+        poster,
+        director,
+        description,
+        id,
+      });
+
+      await user.save();
+      return res.status(201).json({ message: "movie added to list" });
+    }
+  } catch (error) {
+    return res.status(401).json(error);
+  }
+};
---
+++ src/schemas/movie-schemas/index.ts
@@ -0,0 +1,1 @@
+export { default as addMovieSchema } from "./add-movie-schema";
