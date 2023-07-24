a0819cc0a28b03901b2e652f1f9b118a52ff9603
Nika Nozadze <nikanozadze1010@gmail.com>
2023-07-24 17:44:07 +0400

    feat: create route & swagger request for user update

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,44 +1,50 @@
-import { updateAvatar } from "../controllers";
+import { updateAvatar, updateUser } from "../controllers";
 import express, { Request } from "express";
 import { authMiddleware } from "../middlewares";
-import multer, { FileFilterCallback } from 'multer';
+import multer, { FileFilterCallback } from "multer";
 import { DestinationCallback, FileNameCallback } from "types";
 
 const fileStorage = multer.diskStorage({
-    destination: (
-      _: Request,
-      __: Express.Multer.File,
-      callback: DestinationCallback
-    ): void => {
-      callback(null, 'public/storage')
-    },
-    filename: (
-      _: express.Request,
-      file: Express.Multer.File,
-      callback: FileNameCallback
-    ): void => {
-      callback(null, new Date().toISOString() + file.originalname)
-    },
-  })
-  
-  const fileFilter = (
-    __: express.Request,
-    file: Express.Multer.File,
-    callback: FileFilterCallback
+  destination: (
+    _: Request,
+    __: Express.Multer.File,
+    callback: DestinationCallback
   ): void => {
-    if (
-      file.mimetype === 'image/png' ||
-      file.mimetype === 'image/jpg' ||
-      file.mimetype === 'image/jpeg'
-    ) {
-      callback(null, true)
-    } else {
-      callback(null, false)
-    }
+    callback(null, "public/storage");
+  },
+  filename: (
+    _: express.Request,
+    file: Express.Multer.File,
+    callback: FileNameCallback
+  ): void => {
+    callback(null, new Date().toISOString() + file.originalname);
+  },
+});
+
+const fileFilter = (
+  __: express.Request,
+  file: Express.Multer.File,
+  callback: FileFilterCallback
+): void => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    callback(null, true);
+  } else {
+    callback(null, false);
   }
+};
 
 const userRouter = express.Router();
 
-userRouter.put("/user/avatar/:userId", authMiddleware, multer({ storage: fileStorage, fileFilter }).single('avatar'), updateAvatar)
+userRouter.put(
+  "/user/avatar/:userId",
+  authMiddleware,
+  multer({ storage: fileStorage, fileFilter }).single("avatar"),
+  updateAvatar
+);
+userRouter.put("/user/:id", authMiddleware, updateUser);
 
 export default userRouter;
