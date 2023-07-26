a89fd4d71af74ff154b0872009cfd049cde7608e
nixena <nikanozadze1010@gmail.com>
2023-07-26 15:51:34 +0400

    wip: work with movie update request

--- src/controllers/movie-controller.ts
+++ src/controllers/movie-controller.ts
@@ -49,3 +49,50 @@ export const addMovie = async (req: Request, res: Response) => {
     return res.status(401).json(error);
   }
 };
+
+export const updateMovie = async (req: Request, res: Response) => {
+  try {
+    const { body } = req;
+    const id = req.params.id;
+    const { file } = req;
+
+    if (!file) {
+      return res.status(400).json({ message: "image did not upload" });
+    }
+    const transform = {
+      year: body.year,
+      name: { eng: body.name_eng, geo: body.name_geo },
+      director: { eng: body.director_eng, geo: body.director_geo },
+      description: { eng: body.description_eng, geo: body.description_geo },
+      poster: "/storage/" + file.filename,
+      categories: typeof body.categories === "string" ? body.categories.split(",") : body.categories
+    };
+
+    const validator = await addMovieSchema(transform);
+
+    const { name, categories, year, poster, director, description } =
+      await validator.validateAsync(transform);
+
+    const user = await User.findOne({ "movies.id": id});
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "movie did'not find" });
+    }
+
+    const movieIndex = user.movies.findIndex(movie => movie.id === id);
+  
+    if( movieIndex >= 0) {
+      user.movies[movieIndex].name = name;
+      user.movies[movieIndex].categories = categories;
+      user.movies[movieIndex].year = year;
+      user.movies[movieIndex].director = director;
+      user.movies[movieIndex].poster = poster;
+      user.movies[movieIndex].description = description;
+    }
+      await user.save();
+      return res.status(204).json({ message: "movie updated" });
+    } catch (error) {
+    return res.status(401).json(error);
+  }
+};
