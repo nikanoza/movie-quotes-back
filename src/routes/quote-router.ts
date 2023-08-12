15fc1bf814ea01a3c50c5440078380e483e8c525
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-12 11:39:45 +0400

    feat: create

--- src/routes/quote-router.ts
+++ src/routes/quote-router.ts
@@ -1,5 +1,5 @@
 import { authMiddleware } from "../middlewares";
-import { addQuote } from "../controllers";
+import { addQuote, editQuotes } from "../controllers";
 import express from "express";
 import multer from "multer";
 import { fileStorage, fileFilter } from "../types/multer";
@@ -11,6 +11,13 @@ quoteRouter.post(
   authMiddleware,
   multer({ storage: fileStorage, fileFilter }).single("poster"),
   addQuote
+);
+
+quoteRouter.put(
+  "/quotes",
+  authMiddleware,
+  multer({ storage: fileStorage, fileFilter }).single("poster"),
+  editQuotes
 );
 
 export default quoteRouter;
